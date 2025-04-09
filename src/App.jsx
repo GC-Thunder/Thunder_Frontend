928d38f13cb5b0658b606bf8eb9de40ca37e5668
anshkarwasra <anshkarwasra15@gmail.com>
2025-04-09 14:31:41 +0530

    feat:added gradient to homepage and fixed the tailwindcss according to latest docs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+
 import "./index.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ChatArea from './components/chatarea';
-// import "./index.css";
+// import ChatArea from './components/chatarea';
+import Home from './pages/home';
+
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Routes>
         {/* <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} /> */}
-        <Route path='/' element={<ChatArea/>}/>
+        <Route path='/' element={<Home/>}/>
       </Routes>
     </Router>
   );
---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div class="bg-[linear-gradient(0deg,_#1067e899_0%,_#e9e9e9_30%,_#e9e9e9_80%,_#1067e899_100%)]  h-[100vh] w-full">
+  
+</div>
+
+
+  )
+}
+
+export default Home
